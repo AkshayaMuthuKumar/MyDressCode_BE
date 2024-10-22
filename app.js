785302286ae66353e24358531538beaee3bc2cc6
Akshaya Muthukumar <akshaya1907@gmail.com>
2024-10-22 20:08:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.json()); // Parse JSON request bodies
 app.use('/api/products', productRoutes);
 app.use('/api/users', loginRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.post('/create-order', async (req, res) => {
   const { amount, currency } = req.body; // You can pass the amount and currency from frontend
