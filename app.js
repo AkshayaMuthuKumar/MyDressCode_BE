1941d7440d402380e26f166c6ef1a30b54c58c93
Akshaya Muthukumar <akshaya1907@gmail.com>
2024-11-05 12:56:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const Razorpay = require('razorpay');
 require('dotenv').config();
-//Oct15
-
+const path = require('path');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
 const productRoutes = require('./routes/product');
 const loginRoutes = require('./routes/user');
 const { getCurrentUser } = require('./controller/user');
+app.use(bodyParser.json({ limit: '10mb' })); // Adjust the size as needed
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' })); //
 
-const path = require('path');
-
-const cors = require('cors');
-
-const app = express();
 
 const razorpay = new Razorpay({
   key_id: 'rzp_test_OK03rE3KWdrU3p',
@@ -25,7 +24,7 @@ app.use(express.json()); // Parse JSON request bodies
 app.use('/api/products', productRoutes);
 app.use('/api/users', loginRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 
 app.post('/api/create-order', async (req, res) => {
   const { amount, currency } = req.body; // You can pass the amount and currency from frontend
